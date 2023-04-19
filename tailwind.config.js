0cb380cb02361cbba2b779f3658b151d2bc93a22
helpotters <helpotters@penguin>
2023-04-19 00:15:23 -0700

    style: added inter and rubik fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: "Inter",
+        rubik: "Rubik"
+      }
+    }
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
