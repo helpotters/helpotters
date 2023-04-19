0b182e4f00e599e88dc4a408032f48e2665a1000
helpotters <helpotters@penguin>
2023-04-18 18:38:59 -0700

    feat: added avo resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount Avo::Engine, at: Avo.configuration.root_path
+  authenticate :user, ->(user) { user.admin? } do
+    mount Avo::Engine, at: Avo.configuration.root_path
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "home#resume"
+  root 'home#resume'
 end
