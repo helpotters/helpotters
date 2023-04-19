e317178b8aae4563af56e7a198498aefef9570a0
helpotters <helpotters@penguin>
2023-04-18 19:39:32 -0700

    chore: temporarily removed avo mount point

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  authenticate :user, ->(user) { user.admin? } do
-    mount Avo::Engine, at: Avo.configuration.root_path
-  end
-
+  mount Avo::Engine, at: Avo.configuration.root_path
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#resume'
 end
