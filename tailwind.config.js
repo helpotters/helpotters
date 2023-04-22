6ecd6485b63f2f9d750b007335d7c1429af7b889
helpotters <helpotters@penguin>
2023-04-22 15:52:04 -0700

    style: added on-scroll fade animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  plugins: [
+    require('taos/plugin')
+  ],
+ safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    transform: (content) => content.replace(/taos:/g, ''),
+  },
   theme: {
     extend: {
       fontFamily: {
