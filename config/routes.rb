dfd3619818c3b066b7dafb18a0a83c33293d9470
helpotters <helpotters@penguin>
2023-04-24 01:56:15 -0400

    feat: added admin role

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   mount Avo::Engine, at: Avo.configuration.root_path
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#resume'
