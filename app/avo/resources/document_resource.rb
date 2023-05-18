6209f00aa887fcc655560765d3e54be1f1297787
Paul Lemus <paullemus@protonmail.com>
2023-05-18 15:04:03 -0700

    refactor: adjusted appearance of document resource

--- app/avo/resources/document_resource.rb
+++ app/avo/resources/document_resource.rb
@@ -1,14 +1,11 @@
 class DocumentResource < Avo::BaseResource
-  self.title = :id
+  self.title = :name
   self.includes = []
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
-  field :id, as: :id
-  # Fields generated from the model
   field :name, as: :text
   field :public, as: :boolean, name: 'Made Public'
-  # add fields here
   field :pdf, as: :file, accept: '.pdf ', display_filename: true
 end
